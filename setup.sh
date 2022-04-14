128b9a320e2aabd6c145df705f0612b0668442a1
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-04-15 00:10:47 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,7 @@
 mkdir -p ~/.streamlit/
 
 pip install streamlit
+pip install git+https://github.com/deepset-ai/haystack.git
 pip install torch==1.9.0+cu111 torchvision==0.10.0+cu111 torchaudio===0.9.0 -f https://download.pytorch.org/whl/torch_stable.html
 
 echo "\
