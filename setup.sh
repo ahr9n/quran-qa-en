90b6bd5b86081c90cf06c30e8d759ba3481e4345
Ahmad Abdulrahman <52632898+ahr9n@users.noreply.github.com>
2022-04-14 23:59:01 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,2 +1,16 @@
-python3.8 -m pip install torch==1.7.0+cpu torchvision==0.8.1+cpu torchaudio==0.7.0 -f https://download.pytorch.org/whl/torch_stable.html
+mkdir -p ~/.streamlit/
+
+pip install streamlit
 pip install git+https://github.com/deepset-ai/haystack.git
+
+echo "\
+[general]\n\
+email = \"juan.ciro@premexcorp.com\"\n\
+" > ~/.streamlit/credentials.toml
+
+echo "\
+[server]\n\
+headless = true\n\
+enableCORS=false\n\
+port = $PORT\n\
+" > ~/.streamlit/config.toml
